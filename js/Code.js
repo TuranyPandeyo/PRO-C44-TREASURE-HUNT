df0e51a2927c97f8ab62263699298158aea21cde
Turany_Pandey <76775755+TuranyPandeyo@users.noreply.github.com>
2021-04-21 23:21:53 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,26 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("black")
+    text("Hint: Always changing, not constant !!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("C U T N I F O N", 700,50)
+    fill("black")
+    text("Hint: Performs a particular task !!", 700,70)
+
+    fill("white")
+    textSize(15)
+    text("A T E D A S B A", 400,250)
+    fill("black")
+    text("Hint: Stores all information !!", 400,270)
+
+}
